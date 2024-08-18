5152b5775642a126669fd0fba214eb5be052ffce
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-08-18 20:40:18 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
-console.log("Welcome to ebook APIs")
+import app from "./src/app";
+
+console.log("Welcome to ebook APIs");
+
+const startServer = () => {
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+        console.log(`Listining on port: ${PORT}`);
+    });
+};
+
+startServer();
