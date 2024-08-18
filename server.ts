a4d9005c73b0b58be147aed5e99acf48bc2c0530
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-08-18 20:56:56 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
+import { config } from "./src/config/config";
 import app from "./src/app";
 
 console.log("Welcome to ebook APIs");
 
 const startServer = () => {
-    const PORT = process.env.PORT || 3000;
+    const PORT = config.port|| 3000;
 
     app.listen(PORT, () => {
         console.log(`Listining on port: ${PORT}`);
