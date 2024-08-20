99e27492f035726e9275c868738a32ce663c2800
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-08-20 14:17:04 +0530

    update book

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBook } from "./bookController"
+import { createBook, updateBook } from "./bookController"
 import path from "path";
 import multer from "multer";
 import authenticate from "../middlewares/authenticate";
@@ -18,6 +18,15 @@ bookRouter.post('/create',
         {name: "file", maxCount: 1},
     ]),
     createBook
+);
+
+bookRouter.patch('/updateBook/:bookId',
+    authenticate,
+    upload.fields([
+        {name: "coverImage", maxCount: 1},
+        {name: "file", maxCount: 1},
+    ]),
+    updateBook
 );
 
 export default bookRouter;
